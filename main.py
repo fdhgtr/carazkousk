03ba3d86c2568d7876105a611305aded1e920a0c
Tobias <159123644+fdhgtr@users.noreply.github.com>
2024-10-23 06:30:45 +0000

    ffsd

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+pole = [1,22,13,45,9,61,37,89,4,]
+print(pole)
+
+
+prvni = pole[0]
+prostedni = pole[len(pole) // 2]
+posledni = pole[-1]
+
+print("první hodnota:", prvni)
+print("prostřední hodnota:", prostedni)
+print("poslední hodnota:", posledni)
+
+pole[5]=34
+print(pole)
+
+pole7 = pole[7]
+print( pole7)
+
+
+print(len(pole))
+
+print(min(pole))
+print(max(pole))
+
+
+
+pole2 = [6,2,10,15,7,9,11,4,3,1]
+print(pole2)
+
+
+
+print(sum(pole+pole2))
+print(len(pole+pole2))
+
+sum = pole2[1] + pole2[5]
+print(sum)
+
+
